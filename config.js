cd8d8583c51c19dc18fcbe3ce1555ab394ee7c5b
emir875897 <135859720+emir875897@users.noreply.github.com>
2023-06-07 19:24:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    token: "TOKEN", 
-    prefix: "PREFIX",
-    owner: "ID"
+    token: "MTExNjAzOTY5MDkyODI3OTY0Mw.GZRfoM.XCsHG_Lk71i0Yin1wKrhn-jAZ1b11d7MnEjXAQ", 
+    prefix: "e-",
+    owner: "1065655430807830599"
 }
 
